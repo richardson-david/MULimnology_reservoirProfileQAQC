33c40fc88c4a42d2b7df80e11d75deca9ebce26b
richardson-david <richardsond@newpaltz.edu>
2023-09-10 23:01:12 -0400

    Updating a few profiles from conversation with Andrea and also updating the Level 03 with graphical output

--- 05_Scripts/03.00_MULimnology_reservoirProfileQAQC_Level2toLevel3.R
+++ 05_Scripts/03.00_MULimnology_reservoirProfileQAQC_Level2toLevel3.R
@@ -75,7 +75,7 @@ summary3<-profiles2%>%
     epilimnion_temp_degC=mean(temp_degC[depth_m<=top_metalimnion_m],na.rm=TRUE), #average temperature above metalimnion top
     hypolimnion_temp_degC=mean(temp_degC[depth_m>=bottom_metalimnion_m],na.rm=TRUE), #average temperature below the metalimnion bottom
     above_thermocline_temp_degC=mean(temp_degC[depth_m<=thermoclineDepth_m_thresh0.3],na.rm=TRUE), #average temperature above or equal to the thermocline
-    below_thermocline_temp_C=mean(temp_degC[depth_m>thermoclineDepth_m_thresh0.3],na.rm=TRUE), #average temperature below the thermocline
+    below_thermocline_temp_degC=mean(temp_degC[depth_m>thermoclineDepth_m_thresh0.3],na.rm=TRUE), #average temperature below the thermocline
     delta_hypo_epi_waterDensity_kgperm3=water.density(hypolimnion_temp_degC)-water.density(epilimnion_temp_degC), #density difference between hypolimnion water and epilimnion water, bigger difference is stronger stratification
     metalimnionDensityGradient_kgperm3perm=densityGradientAcrossMetalimnion(Depth_vector=depth_m,Temp_vector = temp_degC,meta_top =top_metalimnion_m,meta_bottom = bottom_metalimnion_m), #Function that calculates the water density gradient from the temperate at the top of the metalimnion relative to the bottom correcting for distance between those two measurements######
     epiToHypoDensityGradient_kgperm3perm=densityGradientAcrossEpiToHypo(maxDepth_m=maxDepth_m,epilimnion_temp_degC=epilimnion_temp_degC,hypolimnion_temp_degC=hypolimnion_temp_degC,meta_top =top_metalimnion_m,meta_bottom = bottom_metalimnion_m), #Function that calculates the water density gradient from average epilimnion temperature relative to the bottom, correcting for the distance between the midpoints of the two layers######
@@ -96,7 +96,7 @@ summary3<-profiles2%>%
     Hypoxycline_m=Oxycline_threshold(depth.array=depth_m,DO.array=doConcentration_mgpL,threshold=2), #find the first depth where DO is less than or equal to the threshold of 2, NA means no DO values (all NAs) OR no values below threshold
     Anoxycline_m=Oxycline_threshold(depth.array=depth_m,DO.array=doConcentration_mgpL,threshold=1), #find the first depth where DO is less than or equal to the threshold of 2, NA means no DO values (all NAs) OR no values below threshold
     depthChlMax_m=depthDOmax(depth_vector=depth_m,do_vector=chlorophyll_RFU), #depth of the chlorophyll maximum
-    depthBGMax_m=depthDOmax(depth_vector=depth_m,do_vector=phycocyaninBGA_RFU), #depth of the phycocyanin BGA maximum
+    depthBGAMax_m=depthDOmax(depth_vector=depth_m,do_vector=phycocyaninBGA_RFU), #depth of the phycocyanin BGA maximum
     ratioMaxBGtochl_RFUperRFU=pigmentRatios(phycocyaninBGA_RFU,chlorophyll_RFU,depth_m), #the ratio of the BG value at its max to the chl value at its max
     epilimnion_chlorophyll_RFU=mean(chlorophyll_RFU[depth_m<=top_metalimnion_m],na.rm=TRUE), #average chlorophyll above the top of the metalimnion
     epilimnion_phycocyaninBGA_RFU=mean(phycocyaninBGA_RFU[depth_m<=top_metalimnion_m],na.rm=TRUE), #average chlorophyll above the top of the metalimnion
@@ -119,3 +119,49 @@ summary3<-profiles2%>%
       
 #Export the level3 summary file####
 write_csv(summary3,file=paste0("03_Level3_Data/",yearIndex,"_Level3.csv"))
+
+
+#tick through the profiles and graph them in a 4 panel graph####    
+#profile.index<-1
+for(profile.index in 1:nrow(summary3)){
+  temp_summary<-summary3[profile.index,]
+  temp_profile<-profiles2%>%filter(MULakeNumber==temp_summary$MULakeNumber&date==temp_summary$date)
+  #GG1: Plot of temp vs. depth####
+  gg1<-ggplot(temp_profile%>%arrange(depth_m),aes(y=depth_m,x=temp_degC))+geom_point()+geom_path()+scale_y_reverse()+
+    theme_bw()+
+    labs(x=bquote(Temp~(degree*C)),y="Depth (m)")+
+    geom_hline(yintercept=c(temp_summary$top_metalimnion_m,temp_summary$thermoclineDepth_m_thresh0.3,temp_summary$bottom_metalimnion_m),color=c("red","purple","blue"))+
+    geom_segment(data=temp_summary, aes(x=above_thermocline_temp_degC,xend=above_thermocline_temp_degC,y=thermoclineDepth_m_thresh0.3,yend=minDepth_m),color="purple")+
+    geom_segment(data=temp_summary, aes(x=below_thermocline_temp_degC,xend=below_thermocline_temp_degC,y=thermoclineDepth_m_thresh0.3,yend=maxDepth_m),color="purple")+
+    geom_segment(data=temp_summary, aes(x=epilimnion_temp_degC,xend=epilimnion_temp_degC,y=top_metalimnion_m,yend=minDepth_m),color="red")+
+    geom_segment(data=temp_summary, aes(x=hypolimnion_temp_degC,xend=hypolimnion_temp_degC,y=bottom_metalimnion_m,yend=maxDepth_m),color="blue")
+  
+  #GG2: Plot of DO vs. depth####
+  gg2<-ggplot(temp_profile%>%arrange(depth_m),aes(y=depth_m,x=doConcentration_mgpL))+geom_point()+geom_path()+scale_y_reverse()+ #geom_path to connect in order that data appear in data frame
+    theme_bw()+
+    labs(x=bquote(DO~(mg/L)),y="Depth (m)")+
+    geom_hline(yintercept=c(temp_summary$top_metalimnion_m,temp_summary$thermoclineDepth_m_thresh0.3,temp_summary$bottom_metalimnion_m),color=c("red","purple","blue"))+
+    geom_segment(data=temp_summary, aes(x=epilimnion_DO_mgpL,xend=epilimnion_DO_mgpL,y=top_metalimnion_m,yend=minDepth_m),color="red")+
+    geom_segment(data=temp_summary, aes(x=hypolimnion_DO_mgpL,xend=hypolimnion_DO_mgpL,y=bottom_metalimnion_m,yend=maxDepth_m),color="blue")+
+    geom_hline(yintercept=c(temp_summary$Oxycline_m,temp_summary$Hypoxycline_m,temp_summary$Anoxycline_m),color=c("light grey","grey","dark grey"))+
+    geom_hline(yintercept=c(temp_summary$depthMaxDOpercentage_m),color=c("green"))
+    
+  #GG3: Plot of chl/bga vs. depth####
+  gg3<-ggplot(temp_profile%>%arrange(depth_m),aes(y=depth_m,x=chlorophyll_RFU))+geom_point(color="seagreen3")+geom_path(color="seagreen3")+scale_y_reverse()+ #geom_path to connect in order that data appear in data frame
+    theme_bw()+
+    labs(x=bquote(Photopigment~(RFU)),y="Depth (m)")+
+    geom_path(aes(x=phycocyaninBGA_RFU),color="turquoise")+
+    geom_point(aes(x=phycocyaninBGA_RFU),color="turquoise")+
+    geom_hline(yintercept=c(temp_summary$top_metalimnion_m,temp_summary$thermoclineDepth_m_thresh0.3,temp_summary$bottom_metalimnion_m),color=c("red","purple","blue"))+
+    geom_hline(yintercept=c(temp_summary$depthMaxDOpercentage_m,temp_summary$depthChlMax_m,temp_summary$depthBGAMax_m),color=c("green","seagreen3","turquoise"),size=c(0.5,1.5,1.5))
+    
+  #GG4: Plot of other potentially interesting things vs. depth####
+  gg4<-ggplot(temp_profile%>%arrange(depth_m),aes(y=depth_m,x=turbidity_FNU))+geom_point(color="tan2")+geom_path(color="tan2")+scale_y_reverse()+ #geom_path to connect in order that data appear in data frame
+    theme_bw()+
+    labs(x=bquote(Turbidity~(RFU)~or~pH),y="Depth (m)")+
+    geom_path(aes(x=pH),color="black")+
+    geom_point(aes(x=pH),color="black")+
+    geom_hline(yintercept=c(temp_summary$top_metalimnion_m,temp_summary$thermoclineDepth_m_thresh0.3,temp_summary$bottom_metalimnion_m),color=c("red","purple","blue"))
+    
+  ####STOPPED HERE: CONSTRUCT a 4 panel version, then print on multiple page pdf for each year####
+}    
