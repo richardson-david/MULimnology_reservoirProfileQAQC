4471a1cea9e118239a1674558337619badf7956e
richardson-david <richardsond@newpaltz.edu>
2024-01-17 00:10:36 -0500

    Updating with a calculation of E0

--- 05_Scripts/00_MULimnology_reservoirProfileQAQC_Functions.R
+++ 05_Scripts/00_MULimnology_reservoirProfileQAQC_Functions.R
@@ -301,3 +301,204 @@ pigmentRatios<-function(BGA_vector,CHL_vector,Depth_vector){
 ####End of pigmentRatios####
 
 
+#Function reflectance####
+#Finds the reflection of the sun for a given date and location (from phytotools package)
+#Timezone is relative to GMT, I think 
+reflectance <- function(date,latitude,longitude,timezone){
+  
+  #Calculate sunvector for each date/time
+  sv <- sunvector(JD(date),latitude,longitude,timezone)
+  
+  #Calculate corresponding zenith angle
+  zenith <- sunpos(sv)[,2]
+  
+  #Reflectance is calculated as a function of zenith angle following Kirk 2011
+  zen <- c(seq(0,85,5),87.5,89) 
+  r   <- c(0.020,0.020,0.020,0.020,0.020,0.021,0.021,0.022,0.024,0.028,
+           0.033,0.043,0.059,0.086,0.133,0.211,0.347,0.583,0.761,0.896)
+  
+  reflectance <- approx(zen,r,xout=zenith,rule=2)$y
+  
+  
+  #Compute decimal day
+  decday <- strptime(date,format="%Y-%m-%d %H:%M")$yday + 
+    strptime(date,format="%Y-%m-%d %H:%M")$hour/24 + 
+    strptime(date,format="%Y-%m-%d %H:%M")$min/24/60
+  
+  return(cbind(decday, reflectance))
+  
+}
+
+#Function incident - this is Enaught####
+#Finds the PAR for a given date and location (from phytotools package)
+#Timezone is relative to GMT, I think 
+incident <- function(date, latitude, longitude, elevation,
+                     timezone, meanPAR, TL=3.5, reflectance=TRUE){
+  
+  #Calculate sunvector and sunposition for each date/time
+  sv <- sunvector(JD(date),latitude,longitude,timezone)
+  
+  sp <- sunpos(sv)
+  
+  ######################
+  #Implement Hofierka and Suri 2002
+  ######################
+  
+  #Calculate Decimal Day of Year
+  decday <- strptime(date,format="%Y-%m-%d %H:%M")$yday + 
+    strptime(date,format="%Y-%m-%d %H:%M")$hour/24 + 
+    strptime(date,format="%Y-%m-%d %H:%M")$min/24/60
+  
+  #Calculate solar constant (SC)
+  SC        <- 1367.13 * (1 + 0.03344 *cos(2*pi*decday/365.25-0.048869))
+  
+  #Calculate solar altitude (ho) in degrees
+  ho <- 90-sunpos(sv)[,2]
+  
+  #Calculate corrected solar altitude (horef) due to atomspheric refraction. 
+  #Eq. 6 in Hofierka and Suri
+  horef <- ho + 0.061359*(0.1594+1.123*ho+0.065656*ho*ho)/(1+28.9344*ho+277.3971*ho*ho)
+  
+  #Negative solar altitudes are assigned 0
+  ho[ho<0]       <- 0
+  horef[horef<0] <- 0
+  
+  #Calculate relative optical air mass (m). Eq 5 in Hofierka and Suri
+  m <- exp(-elevation/8434.5)/(sin(horef*pi/180)+0.50572*(horef+6.07995)^(-1.6364)) 
+  
+  #Calculate Rayliegh optical thickness(delr). Eq 8-9 in Hofierka and Suri 
+  delr        <- 1/(6.6296 + 1.7513*m - 0.1202*m^2 + 0.0065*m^3- 0.00013*m^4)
+  delr[m>20]  <- 1/(10.4 + 0.718*m[m>20])
+  
+  #Calculate beam irradiance (B)
+  B   <- SC * exp(-0.8662 * TL * m * delr)
+  B   <- B * sin(ho*pi/180)
+  
+  #Calculate Diffuse radiation (D)
+  Tn <- -0.015843 + 0.0305843*TL - 0.0003797*TL*TL
+  
+  #Coefficients. Eq 24 in Hofierka and Suri
+  A1 <- 0.26463 - 0.061581*TL + 0.0031408*TL*TL
+  A1[A1<0.0022]  <- 0.0022/Tn[A1<0.0022]
+  A2 <- 2.04020 + 0.018945*TL - 0.011161*TL*TL
+  A3 <- -1.3025 + 0.039231*TL + 0.0085079*TL*TL
+  
+  #Calculate diffuse solar radiation function (Fd). Eq. 23 in Hofierka and Suri
+  Fd <- A1+A2*sin(ho*pi/180)+A3*sin(ho*pi/180)^2
+  
+  D  <- SC*Tn*Fd
+  #Set nocturnal values to 0
+  D[ho==0] <- 0
+  
+  #Total incident irradiance sum of beam and diffuse radiation
+  E0    <- B + D
+  #Convert shortwave radiation to PAR, W/m2 to umol m-2 s-1
+  E0    <-  E0 * 4.6 * 0.445  
+  
+  ######################
+  #Scale PAR to user data
+  ######################
+  
+  #Scale E0 to user value if it exists
+  if (missing("meanPAR")){
+    
+    #No value passed so do nothing
+    
+  } else {  
+    
+    #Determine number of simulation days
+    n  <- difftime(max(date),min(date),"days")
+    n  <- as.numeric(n)
+    
+    #Integrate E0
+    fn    <- splinefun(JD(date),E0,method="periodic")    
+    meanE <- integrate(fn,min(JD(date)),max(JD(date)))$value / n
+    
+    #Scale
+    E0 <- E0/meanE*meanPAR
+    
+  }
+  
+  #Subtract surface reflectance if desired following Kirk 2011
+  if (reflectance==TRUE){
+    ref <- reflectance(date,latitude,longitude,timezone)
+    E0 <- E0 - E0*ref[,2] 
+  }
+  
+  #Just return the EO
+  #Previously, they returned both decimal day and E0 using return(cbind(decday,E0))
+  return(E0)
+  
+}
+
+#Function EO_dailyMean####
+#Finds the PAR average for a given date and location
+#Default time-zone is Missouri and elevation is 244 (average for the state, I think)
+#Some trial ones to put in there
+  #date<-Level4_allData[330:331,"date"]%>%pull()
+  #latitude<-Level4_allData[330:331,"site_latitude"]%>%pull()
+  #longitude<-Level4_allData[330:331,"site_longitude"]%>%pull()
+  #elevation_m<-Level4_allData[330:331,"site_altitude_m"]%>%pull()
+EO_dailyMean<-function(date,latitude,longitude,elevation_m=244,timezone=-6){
+  #Create a vector of date times from midnight on that day to 24 hours later, by 10 minutes
+  date_vector<-seq(ISOdate(year(date),month(date),day(date),0,0,0,tz="UTC"),
+            ISOdate(year(date+1),month(date+1),day(date+1),0,0,0,tz="UTC"),by="10 min")
+  #If we are missing elevation/latitude, or longitude, don't calculate incident####
+  if(is.na(latitude)|is.na(longitude)|is.na(elevation)){E0_umolpm2s=NA}else{
+    #Calculate the vector of PARs for that day
+    PARvector<-incident(date=date_vector, 
+                        latitude=latitude, longitude=longitude, 
+                        elevation=elevation_m, timezone=timezone,reflectance=TRUE)
+    #Store the calculated mean PAR in a new variable in the dataframe####
+    E0_umolpm2s<-mean(PARvector)
+  } #end of else
+  
+  return(E0_umolpm2s) #return the mean E0
+    
+}
+
+#Better vectored version####
+EO_dailyMean2<-function(date,latitude,longitude,elevation_m=244,timezone=-6){
+  E0_umolpm2s<-ifelse(is.na(latitude)|is.na(longitude)|is.na(elevation),NA,{ #catch things without lat/long/elev
+    PARvector=0 #Initialize the summation of the par vector for the 24 hour period####
+    for(index in 0:144){
+      ISOdate_new<-ISOdate(year(date),month(date),day(date),0,0,0,tz="UTC")+600*(index+0.016)  #Add 1 second offset to make sure we get midnight
+      PAR_new<-incident(date=ISOdate_new, 
+                      latitude=latitude, longitude=longitude, 
+                      elevation=elevation_m, timezone=timezone,reflectance=TRUE)
+      PARvector<-PARvector+PAR_new #Get a running tally of the PAR
+      }
+      PARvector/(index+1) #Take the arithmetic mean of par
+      
+  }) #end of ifelse
+  return(E0_umolpm2s) #return the mean E0
+} #end of the function
+ 
+#Better individual version- run the for loop outside####
+EO_dailyMean_single<-function(date,latitude=38.377955,longitude=-92.411306,elevation_m=244,timezone=-6){
+  #set E0 to NA 
+  E0_umolpm2s<-NA
+  #Do nothing if there is one of these things missing####
+      if(is.na(latitude)|is.na(longitude)|is.na(elevation_m)){ }else{
+        PARvector<-0 #Initialize the summation of the par vector for the 24 hour period####
+        for(day_index in 0:144){ #If the profile has long, lat, and elev, then 
+          ISOdate_new<-ISOdate(year(date),month(date),day(date),0,0,0,tz="UTC")+600*(day_index+0.016)  #Add 1 second offset to make sure we get midnight
+          #print(ISOdate_new)
+          PAR_new<-incident(date=ISOdate_new, 
+                            latitude=latitude, longitude=longitude, 
+                            elevation=elevation_m, timezone=timezone,reflectance=TRUE)
+          PARvector<-PARvector+PAR_new #Get a running tally of the PAR
+          #print(PARvector)
+        } #end of for loop
+        E0_umolpm2s<-PARvector/(day_index+1) #Take the arithmetic mean of par 
+      }  
+    
+  #print(E0_umolpm2s)      
+  return(E0_umolpm2s) #return the mean E0
+    
+} #end of the function
+
+
+
+
+
--- 05_Scripts/04.00_MULimnology_reservoirProfileQAQC_ImportAndMergeSecchi.R
+++ 05_Scripts/04.00_MULimnology_reservoirProfileQAQC_ImportAndMergeSecchi.R
@@ -22,6 +22,15 @@ secchiDF_1976_2022<-read_csv(file=paste0(dirPathSecchi,"/","1976-2022_SLAP_Secch
                           )%>%
                     dplyr::select(MULakeNumber,date,SecchiDepth_m)
 
+  #Check the original Secchi upload to see about some individual data rows####
+  tempSecchi<-read_csv(file=paste0(dirPathSecchi,"/","1976-2022_SLAP_SecchiDepth_Masterfile_01-11-2024.csv"), col_types = cols())%>% #last argument suppresses the message on input about column types, helpful for mass upload
+    mutate(
+      MULakeNumber=sprintf("%03d", MULakeNumber),
+      date=as_date(mdy_hm(beginDateTime))
+    )
+  #Filter out specific sites here####
+  tempSecchi%>%filter(MULakeNumber==438)%>%print(n=Inf)
+
 #Read in the Secchi data from 2023 only####
 secchiDF_2023<-read_csv(file=paste0(dirPathSecchi,"/","2023_SLAP_SecchiDepth_Masterfile_01-11-2024.csv"), col_types = cols())%>% #last argument suppresses the message on input about column types, helpful for mass upload
   mutate(
@@ -31,7 +40,10 @@ secchiDF_2023<-read_csv(file=paste0(dirPathSecchi,"/","2023_SLAP_SecchiDepth_Mas
   dplyr::select(MULakeNumber,date,SecchiDepth_m)
 
 #Merge the two Secchis together (include other years here)####
-secchiDF<-bind_rows(secchiDF_1976_2022,secchiDF_2023)
+secchiDF<-bind_rows(secchiDF_1976_2022,secchiDF_2023)%>%
+            group_by(MULakeNumber,date)%>%
+            summarize(SecchiDepth_m=mean(SecchiDepth_m))%>% #*Collapse any replicates for date/MULakeNumber by taking the mean####       
+            ungroup()
 
 #*Set the directory path here####
 dirPath_Level03<-paste0("03_Level3_Data/")
@@ -51,8 +63,32 @@ for(fileIndex in 1:length(Level3_files)){
 #Bind them all together####
 Level3_allData<-do.call(bind_rows, List_Level3)
 
+#Merge Secchi with Level3_alldata (Level4_alldata)####
+  #*Check to see what secchis have a profile####
+  temp<-left_join(secchiDF,Level3_allData%>%dplyr::select(MULakeNumber,date,numberOfMeasurements_temperature),by=c("MULakeNumber","date"))
+  temp%>%filter(is.na(numberOfMeasurements_temperature))
+
+#Join the level 3 data with Secchi data frame  
+Level4_allData<-left_join(Level3_allData,secchiDF,by=c("MULakeNumber","date"))
+#Check to see what profiles are missing a secchi####
+  #*Only 194 out of 10235 profiles have no Secchi
+  Level4_allData%>%filter(is.na(SecchiDepth_m))%>%print(n=Inf)
+
+
+#Calculate E0 for all the days in the data frame####
+#*This step takes a few minutes to run through#
+Level4_allData$E0_umolpm2s<-NA #Initialize E0 column
+for(dateIndex in 1:nrow(Level4_allData)){
+  Level4_allData$E0_umolpm2s[dateIndex]<-EO_dailyMean_single(date=Level4_allData$date[dateIndex],latitude=Level4_allData$site_latitude[dateIndex],longitude=Level4_allData$site_longitude[dateIndex],elevation_m=Level4_allData$site_altitude_m[dateIndex])
+} #End of loop through all the rows
+
 ###STOPPED HERE####
 #Next things to do####
-#Merge Secchi with Level3_alldata (Level4_alldata)
+#Prior to calculating E0 - merge with Lat/Long metadata to get better estimates of Lat Long
+  #Create new column that gets lat/long as the handheld if it exists or the site data if not
+  #Use elevation if it exists or default elevation (244) if not
 #Calculate E0 for each day
+#Calculate kd based on secchi: 
+  #kd_1_m=1.44/Secchi_m
 #Calculate E24 for each day
+  #E24_umolpm2s=E0_umolpm2s*(1-exp(-1*kd_1_m*top_metalimnion_m))*((kd_1_m*top_metalimnion_m)^-1)
