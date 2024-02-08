e2f9fb9b6be39a3f59c6c87737a0e1bd15aa923b
richardson-david <richardsond@newpaltz.edu>
2024-02-08 16:02:57 -0500

    Updating upload with N comparison sheet

--- 05_Scripts/06.00_MULimnology_mergingParameterMasterSheets.R
+++ 05_Scripts/06.00_MULimnology_mergingParameterMasterSheets.R
@@ -15,7 +15,7 @@ dir<-paste0("08_ParameterMasterSheets/",year,"_ParameterMasterSheets")
 
 
 #Read in master parameter sheets####
-
+###############################
 #*Read in chlorophyll sheet####
   #Gives total chlorophyll, Chla corrected and Pheophytin#
 #**Read in Chlorophyll master from data for pivot table sheet####
@@ -39,20 +39,75 @@ chlMaster_All<-read_excel(paste0(dir,"/2023 SLAP chl compiled.xlsx"),sheet="All"
 #**Things to do: get rid of all with "Field Blank" in MULakeNumber
 #**Things to do: get rid of field dupes (FD at the end of MULakeNumber)
 
+###############################
 #*Read in Ammonia from NH sheet####
 #Gives total NH4 concentration#
 
-#**Bring in the chlorophyll from the all sheet####
+#**Bring in the NH4 from the all sheet####
 Ammonium_NH<-read_excel(paste0(dir,"/Ammonia SLAP 2023 mastersheet.xlsx"),sheet="NH",skip=0)%>%
   rename(MULakeNumber=`Lake #`,
          dateTime=Date,
          DepthNH4=Depth,
          NH4_ugpL=`[NH4]` 
          )%>%
-  dplyr::select(MULakeNumber,dateTime,DepthChl,NH4_ugpL)
+  dplyr::select(MULakeNumber,dateTime,DepthNH4,NH4_ugpL)
 
 #**Things to do: check the units for NH4
 #**Things2do: check for detection limits: rule if below?
 #**Things2do: take average from duplicates
+#**Things to do: get rid of all with "Field Blank" in MULakeNumber
+#**Things to do: get rid of field dupes (FD at the end of MULakeNumber)
+
+
+###############################
+#*Read in Nitrogen from across all sheets####
+#Gives TN, TDN, NO3, and NH4 concentrations#
+
+#**Bring in the TN from the TN sheet####
+Ncomp_TN<-read_excel(paste0(dir,"/N comparison SLAP 2023.xlsx"),sheet="TN",skip=0)%>%
+  rename(MULakeNumber=`Lake #`,
+         dateTime=Date,
+         DepthTN=Depth,
+         TN_mgpL=`[N]` 
+  )%>%
+  mutate(date=ymd(dateTime))%>%
+  dplyr::select(MULakeNumber,date,DepthTN,TN_mgpL)
+
+#**Bring in the TDN from the TDN sheet####
+Ncomp_TDN<-read_excel(paste0(dir,"/N comparison SLAP 2023.xlsx"),sheet="DN",skip=0)%>%
+  rename(MULakeNumber=`Lake #`,
+         dateTime=Date,
+         DepthTDN=Depth,
+         TDN_mgpL=`[N]` 
+  )%>%
+  mutate(date=ymd(dateTime))%>%
+  dplyr::select(MULakeNumber,date,DepthTDN,TDN_mgpL)
+
+#**Bring in the NO3 from the NO3 sheet####
+Ncomp_NO3<-read_excel(paste0(dir,"/N comparison SLAP 2023.xlsx"),sheet="NO3",skip=0)%>%
+  rename(MULakeNumber=`Lake #`,
+         dateTime=Date,
+         DepthNO3=Depth,
+         NO3_mgpL=`[NO3] mg/L` 
+  )%>%
+  mutate(date=ymd(dateTime))%>%
+  dplyr::select(MULakeNumber,date,DepthNO3,NO3_mgpL)
+
+
+#**Bring in the NH4 from the NH4 sheet####
+Ncomp_NH4<-read_excel(paste0(dir,"/N comparison SLAP 2023.xlsx"),sheet="NH4",skip=0)%>%
+  rename(MULakeNumber=`Lake #`,
+         dateTime=Date,
+         DepthNH4=Depth,
+         NH4_mgpL=`[NH4]` 
+  )%>%
+  mutate(date=ymd(dateTime))%>%
+  dplyr::select(MULakeNumber,date,DepthNH4,NH4_mgpL)
+
+
+#**Things to do: check the units for TN, TDN, NO3, NH4
+#**Things2do: check for detection limits: rule if below?
+#** Check for comparison between different nitrogens
+#**Things2do: take average from duplicates before merging
 #**Things to do: get rid of all with "Field Blank" in MULakeNumber
 #**Things to do: get rid of field dupes (FD at the end of MULakeNumber)
